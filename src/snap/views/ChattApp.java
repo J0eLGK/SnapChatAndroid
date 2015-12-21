3bed227de902c4cbfa890bd564d68ba140e356e7
dimitrisKK <jim_karad@hotmail.gr>
2015-12-21 02:14:19 +0200

    Create ChattApp.java

---
+++ src/snap/views/ChattApp.java
@@ -0,0 +1,14 @@
+package snap.views;
+
+import android.app.Application;
+
+import com.parse.Parse;
+
+public class ChattApp extends Application{
+	
+	@Override
+	public void onCreate(){
+		super.onCreate();
+		Parse.initialize(this, "XlUoocE4KS8JQkFdPWyhZGtP6ksuHOIzo1VAjij8", "syr0MzRvftQohn776NxmZh6S2mRab5EbrJV5nB32");
+	}
+}
