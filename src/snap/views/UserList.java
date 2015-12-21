d1457d60852a687af734bf44308b074eb69694c5
J0eLGK <GEORGE_TAME@HOTMAIL.COM>
2015-12-21 08:37:00 +0200

    Update UserList.java

--- src/snap/views/UserList.java
+++ src/snap/views/UserList.java
@@ -1,127 +1,149 @@
-package snap.controllers;
+package snap.views;
 
-import android.app.Activity;
-import android.app.AlertDialog;
-import android.content.Context;
-import android.content.DialogInterface;
+import java.util.ArrayList;
+import java.util.List;
+
+import snap.controllers.Const;
+import snap.controllers.Utils;
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
-import android.view.inputmethod.InputMethodManager;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
+import android.widget.BaseAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
 
-public class Utils{
+import com.parse.FindCallback;
+import com.parse.ParseException;
+import com.parse.ParseUser;
+
+public class UserList extends CustomActivity{
+
+	private ArrayList<ParseUser> uList;
+
+	public static ParseUser user;
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState){
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.user_list);
+
+		getActionBar().setDisplayHomeAsUpEnabled(false);
+		
+		setTouchNClick(R.id.btnLogout);
+
+		updateUserStatus(true);
+	}
 	
-	public static AlertDialog showDialog(Context ctx, String msg, String btn1,
-			String btn2, DialogInterface.OnClickListener listener1,
-			DialogInterface.OnClickListener listener2){
-
-		AlertDialog.Builder builder = new AlertDialog.Builder(ctx);
-		builder.setMessage(msg).setCancelable(false)
-				.setPositiveButton(btn1, listener1);
-		if (btn2 != null && listener2 != null)
-			builder.setNegativeButton(btn2, listener2);
-
-		AlertDialog alert = builder.create();
-		alert.show();
-		return alert;
-
-	}
-
-	public static AlertDialog showDialog(Context ctx, int msg, int btn1,
-			int btn2, DialogInterface.OnClickListener listener1,
-			DialogInterface.OnClickListener listener2){
-
-		return showDialog(ctx, ctx.getString(msg), ctx.getString(btn1),
-				ctx.getString(btn2), listener1, listener2);
-
-	}
-
-	public static AlertDialog showDialog(Context ctx, String msg, String btn1,
-			String btn2, DialogInterface.OnClickListener listener){
-
-		return showDialog(ctx, msg, btn1, btn2, listener,
-				new DialogInterface.OnClickListener() {
-					@Override
-					public void onClick(DialogInterface dialog, int id)
-					{
-
-						dialog.dismiss();
-					}
-				});
-
-	}
-
-	public static AlertDialog showDialog(Context ctx, int msg, int btn1,
-			int btn2, DialogInterface.OnClickListener listener){
-
-		return showDialog(ctx, ctx.getString(msg), ctx.getString(btn1),
-				ctx.getString(btn2), listener);
-
-	}
-
-	public static AlertDialog showDialog(Context ctx, String msg,
-			DialogInterface.OnClickListener listener){
-
-		return showDialog(ctx, msg, ctx.getString(android.R.string.ok), null,
-				listener, null);
-	}
-
-	public static AlertDialog showDialog(Context ctx, int msg,
-			DialogInterface.OnClickListener listener){
-
-		return showDialog(ctx, ctx.getString(msg),
-				ctx.getString(android.R.string.ok), null, listener, null);
-	}
-
-	public static AlertDialog showDialog(Context ctx, String msg){
-
-		return showDialog(ctx, msg, new DialogInterface.OnClickListener() {
-			@Override
-			public void onClick(DialogInterface dialog, int id)
-			{
-
-				dialog.dismiss();
-			}
-		});
-
-	}
-
-	public static AlertDialog showDialog(Context ctx, int msg){
-
-		return showDialog(ctx, ctx.getString(msg));
-
-	}
-
-	public static void showDialog(Context ctx, int title, int msg,
-			DialogInterface.OnClickListener listener){
-
-		AlertDialog.Builder builder = new AlertDialog.Builder(ctx);
-		builder.setMessage(msg).setCancelable(false)
-				.setPositiveButton(android.R.string.ok, listener);
-		builder.setTitle(title);
-		AlertDialog alert = builder.create();
-		alert.show();
-	}
-
-	public static final void hideKeyboard(Activity ctx){
-
-		if (ctx.getCurrentFocus() != null)
-		{
-			InputMethodManager imm = (InputMethodManager) ctx
-					.getSystemService(Context.INPUT_METHOD_SERVICE);
-			imm.hideSoftInputFromWindow(ctx.getCurrentFocus().getWindowToken(),
-					0);
+	@Override
+	public void onClick(View v){
+		super.onClick(v);
+		if (v.getId() == R.id.btnLogout){
+			logout();
+			
 		}
 	}
+	
+	private void logout(){
+		ParseUser.logOut();
+		Intent intent = new Intent(this, Login.class);
+		startActivity(intent);
+		this.finish();
+	}
+	
+	@Override
+	protected void onDestroy(){
+		super.onDestroy();
+		updateUserStatus(false);
+	}
 
-	public static final void hideKeyboard(Activity ctx, View v){
+	@Override
+	protected void onResume(){
+		super.onResume();
+		loadUserList();
 
-		try
-		{
-			InputMethodManager imm = (InputMethodManager) ctx
-					.getSystemService(Context.INPUT_METHOD_SERVICE);
-			imm.hideSoftInputFromWindow(v.getWindowToken(), 0);
-		} catch (Exception e)
-		{
-			e.printStackTrace();
+	}
+	
+	private void updateUserStatus(boolean online){
+		user.put("online", online);
+		user.saveEventually();
+	}
+
+	private void loadUserList(){
+		final ProgressDialog dia = ProgressDialog.show(this, null,
+				getString(R.string.alert_loading));
+		ParseUser.getQuery().whereNotEqualTo("username", user.getUsername())
+				.findInBackground(new FindCallback<ParseUser>() {
+
+					@Override
+					public void done(List<ParseUser> li, ParseException e){
+						dia.dismiss();
+						if (li != null){
+							if (li.size() == 0)
+								Toast.makeText(UserList.this,
+										R.string.msg_no_user_found,
+										Toast.LENGTH_SHORT).show();
+
+							uList = new ArrayList<ParseUser>(li);
+							ListView list = (ListView) findViewById(R.id.list);
+							list.setAdapter(new UserAdapter());
+							list.setOnItemClickListener(new OnItemClickListener() {
+
+								@Override
+								public void onItemClick(AdapterView<?> arg0,
+										View arg1, int pos, long arg3){
+									startActivity(new Intent(UserList.this,
+											Chat.class).putExtra(
+											Const.EXTRA_DATA, uList.get(pos)
+													.getUsername()));
+								}
+							});
+						}
+						else{
+							Utils.showDialog(
+									UserList.this,
+									getString(R.string.err_users) + " "
+											+ e.getMessage());
+							e.printStackTrace();
+						}
+					}
+				});
+	}
+	
+	private class UserAdapter extends BaseAdapter{
+
+		@Override
+		public int getCount(){
+			return uList.size();
+		}
+		
+		@Override
+		public ParseUser getItem(int arg0){
+			return uList.get(arg0);
+		}
+		
+		@Override
+		public long getItemId(int arg0){
+			return arg0;
+		}
+
+		@Override
+		public View getView(int pos, View v, ViewGroup arg2){
+			if (v == null)
+				v = getLayoutInflater().inflate(R.layout.chat_item, null);
+
+			ParseUser c = getItem(pos);
+			TextView lbl = (TextView) v;
+			lbl.setText(c.getUsername());
+			lbl.setCompoundDrawablesWithIntrinsicBounds(
+					c.getBoolean("online") ? R.drawable.ic_online
+							: R.drawable.ic_offline, 0, R.drawable.arrow, 0);
+			return v;
 		}
 	}
 }
