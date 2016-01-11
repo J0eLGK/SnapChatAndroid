ddf8534d639384dc389ba53e60a1e5189d7e0c03
dimitrisKK <jim_karad@hotmail.gr>
2016-01-11 02:39:05 +0200

    Update ChattApp.java

--- src/snap/views/ChattApp.java
+++ src/snap/views/ChattApp.java
@@ -3,6 +3,7 @@ package snap.views;
 import android.app.Application;
 
 import com.parse.Parse;
+import com.parse.ParseACL;
 
 public class ChattApp extends Application{
 	
@@ -10,5 +11,6 @@ public class ChattApp extends Application{
 	public void onCreate(){
 		super.onCreate();
 		Parse.initialize(this, "XlUoocE4KS8JQkFdPWyhZGtP6ksuHOIzo1VAjij8", "syr0MzRvftQohn776NxmZh6S2mRab5EbrJV5nB32");
+
 	}
 }
